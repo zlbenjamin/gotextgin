f417bccf3e7209ad1336bd8a6f48d9da68dc4326
zlbenjamin <43943056+zlbenjamin@users.noreply.github.com>
2024-05-27 16:32:54 +0800

    Initialize project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zlbenjamin/gotextgin
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func init() {
+	// Connect to Database
+}
+
+func init() {
+	// Create table
+}
+
+func main() {
+
+}
