396f3288d4b4ad7ea5b04f1321286a62e1f1b4c8
zlbenjamin <43943056+zlbenjamin@users.noreply.github.com>
2024-05-27 16:35:37 +0800

    start web server

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 func init() {
 	// Connect to Database
 }
@@ -9,5 +16,15 @@ func init() {
 }
 
 func main() {
+	r := gin.Default()
 
+	s := &http.Server{
+		Addr:           ":40000",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 2 << 20,
+	}
+
+	s.ListenAndServe()
 }
