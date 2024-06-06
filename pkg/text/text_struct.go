c6bba2e272edea7ca44ca7bb9b34c4dd4a2fbb13
zlbenjamin <43943056+zlbenjamin@users.noreply.github.com>
2024-06-06 12:46:28 +0800

    New tables: table_comment, table_tag

--- pkg/text/text_struct.go
+++ pkg/text/text_struct.go
@@ -13,19 +13,27 @@ const Table_Text_Tag = "text_tag"
 
 // table: text
 type Text struct {
-	Id         int32     `json:"id"`
-	Content    string    `json:"content"`
-	Type       string    `json:"type"`
-	CreateTime time.Time `json:"createTime"`
-	UpdateTime time.Time `json:"updateTime"`
+	Id         int32     `json:"id" gorm:"primaryKey;comment:ID"`
+	Content    string    `json:"content" gorm:"not null;comment:content of a Text;type:MEDIUMTEXT"`
+	Type       string    `json:"type" gorm:"not null;size:100;comment:type of a Text"`
+	CreateTime time.Time `json:"createTime" gorm:"not null;comment:create time;default:CURRENT_TIMESTAMP"`
+	UpdateTime time.Time `json:"updateTime" gorm:"not null;comment:the latest update time;default:CURRENT_TIMESTAMP"`
 }
 
 // table: text_comment
 type TextComment struct {
-	// TODO
+	Id         uint64    `json:"id" gorm:"primaryKey;comment:ID"`
+	TextId     int32     `json:"textId" gorm:"not null;comment:ID of a Text"`
+	Comment    string    `json:"comment" gorm:"not null;size:500;comment:content of comment"`
+	CreateTime time.Time `json:"createTime" gorm:"not null;comment:create time;default:CURRENT_TIMESTAMP"`
+	UpdateTime time.Time `json:"updateTime" gorm:"not null;comment:the latest update time;default:CURRENT_TIMESTAMP"`
 }
 
 // table: text_tag
 type TextTag struct {
-	// TODO
+	Id         uint64    `json:"id" gorm:"primaryKey;comment:ID"`
+	TextId     int32     `json:"textId" gorm:"not null;comment:ID of a Text"`
+	Name       string    `json:"name" gorm:"not null;size:500;comment:Tag name"`
+	CreateTime time.Time `json:"createTime" gorm:"not null;comment:create time;default:CURRENT_TIMESTAMP"`
+	UpdateTime time.Time `json:"updateTime" gorm:"not null;comment:the latest update time;default:CURRENT_TIMESTAMP"`
 }
