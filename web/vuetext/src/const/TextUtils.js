22404c2f7d034b7cad3b6789188b02444ebf5734
zlbenjamin <43943056+zlbenjamin@users.noreply.github.com>
2024-06-13 17:03:47 +0800

    web: Code refactoring

--- web/vuetext/src/const/TextUtils.js
+++ web/vuetext/src/const/TextUtils.js
@@ -1,6 +1,28 @@
 
+// export
 // Process content of a text
 export function processTextContent(content) {
+    if (Object.is(content, null) || Object.is(content, undefined)) {
+        return ''
+    }
+
+    let pcontent = content
+    pcontent = htmlEncode(pcontent)
+
+    // replace \n
+    pcontent = pcontent.replaceAll("\n", "<br>")
+
+    // replace url in content
+    // Note, there are some bugs TODO
+    let urlg = new RegExp("http[s]://[a-zA-Z0-9\.\?/#=_:\\-\\+%]+", "ig")
+    pcontent = pcontent.replaceAll(urlg, replaceUrl)
+
+    return pcontent
+}
+
+// export
+// Process comment of text
+export function processComment(content) {
     if (Object.is(content, null) || Object.is(content, undefined)) {
         return ''
     }
--- web/vuetext/src/views/HomeView.vue
+++ web/vuetext/src/views/HomeView.vue
@@ -4,6 +4,8 @@ import { ElMessage, ElMessageBox } from 'element-plus'
 
 import axios from "axios"
 
+import { processTextContent, processComment } from '@/const/TextUtils.js'
+
 onMounted(() => {
     pageFind()
 })
@@ -254,65 +256,6 @@ const needUpdate = ref(0)
 watch(needUpdate, (val) => {
     pageFind()
 })
-
-// Process content of a text
-function processTextContent(content) {
-    if (Object.is(content, null) || Object.is(content, undefined)) {
-        return ''
-    }
-
-    let pcontent = content
-    pcontent = htmlEncode(pcontent)
-
-    // replace \n
-    pcontent = pcontent.replaceAll("\n", "<br>")
-
-    // replace url in content
-    // Note, there are some bugs TODO
-    let urlg = new RegExp("http[s]://[a-zA-Z0-9\.\?/#=_:\\-\\+%]+", "ig")
-    pcontent = pcontent.replaceAll(urlg, replaceUrl)
-
-    return pcontent
-}
-
-// Process comment
-function processComment(content) {
-    if (Object.is(content, null) || Object.is(content, undefined)) {
-        return ''
-    }
-
-    let pcontent = content
-    pcontent = htmlEncode(pcontent)
-
-    // replace \n
-    pcontent = pcontent.replaceAll("\n", "<br>")
-
-    // replace url in content
-    // Note, there are some bugs TODO
-    let urlg = new RegExp("http[s]://[a-zA-Z0-9\.\?/#=_:\\-\\+%]+", "ig")
-    pcontent = pcontent.replaceAll(urlg, replaceUrl)
-
-    return pcontent
-}
-
-// HTML special characters
-function htmlEncode(content) {
-    content = content.replaceAll("&", "&amp;")
-			
-    content = content.replaceAll("<", "&lt;")
-    content = content.replaceAll(">", "&gt;")
-    content = content.replaceAll("\"", "&quot;")
-    content = content.replaceAll(" ", "&nbsp;")
-
-    content = content.replaceAll("\t", "&emsp;")
-
-    return content
-}
-
-// Replace matched url with <a> element
-function replaceUrl(url) {
-    return "<a href='" + url + "' target='_blank' class='content-url' >" + decodeURI(url) + "</a>"
-}
 
 // Comment
 
